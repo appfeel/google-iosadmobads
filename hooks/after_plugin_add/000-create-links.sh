885ee0969bbf3d1845c1e4cd4e8abfb673de7a93
Miquel Martín <mmgadmin@MacMiquel.local>
2015-03-14 12:05:23 +0100

    Create links only when is installing this plugin

--- hooks/after_plugin_add/000-create-links.sh
+++ hooks/after_plugin_add/000-create-links.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ $CORDOVA_PLUGINS == *"com.ios.libgoogleadmobads"* ]]
+then
+  echo "Creating necessary links for GoogleAdbom.framework SDK..."
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
 
-ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
+
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
 
 
-ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
-
-
-ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
+  
+fi
