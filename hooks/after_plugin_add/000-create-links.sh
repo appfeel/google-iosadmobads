2ab44e8d58cc8c5851f6c1e61db594050b108ba8
Miquel Martín <mmgadmin@MacMiquel.local>
2015-03-16 19:01:48 +0100

    Fix links issue

--- hooks/after_plugin_add/000-create-links.sh
+++ hooks/after_plugin_add/000-create-links.sh
@@ -4,17 +4,17 @@ echo "Creating necessary links for GoogleAdbom.framework SDK..."
 
 if [ ! -e plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers ]
 then
-  ln -rs plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/Headers/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Headers
 fi
 
 if [ ! -e plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds ]
 then
-  ln -rs plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/GoogleMobileAds plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/GoogleMobileAds
 fi
 
 if [ ! -e plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current ]
 then
-  ln -rs plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
+  ln -s -r plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/A/ plugins/com.ios.libgoogleadmobads/src/ios/GoogleMobileAds.framework/Versions/Current
 fi
 
 echo "done!"
